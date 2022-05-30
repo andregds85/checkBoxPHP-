c98fcb32d874a0a439a9be24a1cf5a9c47707c3c
André Gomes da Silva <67976938+andregds85@users.noreply.github.com>
2022-05-30 19:00:48 -0300

    teste
    teste

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+   <input type="checkbox" name="grupo_chk[]" value="1">
+   <input type="checkbox" name="grupo_chk[]" value="2">
+   <input type="checkbox" name="grupo_chk[]" value="3">
+   <input type="checkbox" name="grupo_chk[]" value="4">
+   <br>
+   <br>
+   <input type="submit" name="Enviar" value="Submit">
+</form>
+<?php
+print_r($_POST['grupo_chk']);
+?>
